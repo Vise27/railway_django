0bbd216c07bcea7cef5f6e5a707541589a924130
Marco Vise <marco.vise19@gmail.com>
2024-11-16 13:10:26 -0500

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -50,6 +50,20 @@ REST_FRAMEWORK = {
     ],
 }
 CORS_ALLOW_ALL_ORIGINS = True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 
 CORS_ALLOW_METHODS = [
     'GET',
