94ead55ed094bf3244d860469b3b15b42192c3ca
Marco Vs <marco.vise19@gmail.com>
2024-12-02 03:42:38 -0500

    host actualizado

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -183,3 +183,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS=['elsol.up.railway.app']
